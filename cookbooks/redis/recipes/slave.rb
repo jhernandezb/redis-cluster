5a031d0e87418669bba126374d014a81db870159
jhernandezb <contact@jhernandez.me>
2018-04-25 12:10:11 -0600

    slave bind port

--- cookbooks/redis/recipes/slave.rb
+++ cookbooks/redis/recipes/slave.rb
@@ -6,7 +6,7 @@
 include_recipe 'redis::default'
 docker_container 'redis-slave' do
   repo 'redis'
-  port ['6379:6379', '26379:26379']
+  port ['6379:6379']
   restart_policy 'always'
   volumes '/etc/redis/slave.conf:/etc/redis/slave.conf'
   command "redis-server /etc/redis/slave.conf"
