bb0c84109c46d4af3ca78cdb068c788210ab7eba
jhernandezb <contact@jhernandez.me>
2018-04-25 10:29:36 -0600

    update sentinel port

--- cookbooks/redis/recipes/sentinel.rb
+++ cookbooks/redis/recipes/sentinel.rb
@@ -6,7 +6,7 @@
 include_recipe 'redis::default'
 docker_container 'redis-sentinel' do
   repo 'redis'
-  port ['6379:6379', '26379:26379']
+  port ['26379:26379']
   restart_policy 'always'
   command "redis-server --sentinel"
 end
